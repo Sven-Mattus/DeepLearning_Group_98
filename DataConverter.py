63540fb68c034478abcec22bdca63133f88d50d9
laura <laura.erb1@gmx.de>
2024-05-10 20:01:11 +0200

    split text into sequences

--- DataConverter.py
+++ DataConverter.py
@@ -21,3 +21,11 @@ class DataConverter:
             char_to_ind[book_chars[i]] = i
             ind_to_char[i] = book_chars[i]
         return char_to_ind, ind_to_char
+
+    def chunk_list(self, array, seq_length):
+        return [array[i:i + seq_length] for i in range(0, len(array) - seq_length + 1, seq_length)]
+
+    def split_input_target(self, chunk):
+        input_text = chunk[:-1]
+        target_text = chunk[1:]
+        return input_text, target_text
