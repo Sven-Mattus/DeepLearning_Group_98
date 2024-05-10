430c6339b194186cf02e9f30e67e47005c6a7ea0
laura <laura.erb1@gmx.de>
2024-05-10 19:38:27 +0200

    added DataConverter.py

---
+++ DataConverter.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class DataConverter:
+
+    def __init__(self, book_chars):
+        self._book_chars = book_chars
+        self._char_to_ind, self._ind_to_char = self._generate_convert_dicts(book_chars)
+
+    def chars_to_ind(self, chars):
+        ind = np.zeros(len(chars))
+        for i in range(len(chars)):
+            ind[i] = self._char_to_ind[chars[i]]
+        return ind
+
+    def _generate_convert_dicts(self, book_chars):
+        K = len(book_chars)
+        char_to_ind = {}
+        ind_to_char = {}
+        for i in range(K):
+            char_to_ind[book_chars[i]] = i
+            ind_to_char[i] = book_chars[i]
+        return char_to_ind, ind_to_char
--- Group_Project_Part_1.py
+++ Group_Project_Part_1.py
@@ -1,3 +1,7 @@
+from DataConverter import DataConverter
 from DataLoader import DataLoader
 
 book = DataLoader.load_data()
+book_chars = sorted(set(book))
+data_converter = DataConverter(book_chars)
+
