098e71bae41e9435160a66472b714a3bb761b527
laura <laura.erb1@gmx.de>
2024-05-14 08:36:37 +0200

    completed math_functions.py

--- math_function/math_functions.py
+++ math_function/math_functions.py
@@ -5,7 +5,14 @@ def sigmoid(t):
     sig = 1 / (1 + np.exp(-t))
     return sig
 
+def sigmoid_derivative(t):
+    sig_deriv = sigmoid(t) * (1 - sigmoid(t))
+    return sig_deriv
 
 def tanh(x):
     tanh = (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
     return tanh
+
+def tanh_derivative(t):
+    tanh_deriv = 1 - tanh(t)*tanh(t)
+    return tanh_deriv
