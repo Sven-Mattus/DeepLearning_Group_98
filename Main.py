0548fb0c8c2467705c5c4e5c2c174dafda27cd5e
Andri Furrer <andrifurrer@p54-n140.kthopen.kth.se>
2024-05-22 17:10:17 +0200

    LSTM ready to train - added test_loss, acc, filesaving

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     book_as_ind = data_converter.chars_to_ind(book)
 
     # generate dataset
-    SEQ_LENGTH = 100
+    SEQ_LENGTH = 25
     BATCH_SIZE = 64
 
     # initialize network
@@ -21,21 +21,42 @@ if __name__ == "__main__":
 
     # train LSTM
     validation_set_len = BATCH_SIZE * SEQ_LENGTH * 2
-    NR_EPOCHS = 10
+    test_set_len = validation_set_len + BATCH_SIZE * SEQ_LENGTH * 2
+    NR_EPOCHS = 1
 
-    dataset_input, dataset_target = DataGenerator.create_array_dataset(book_as_ind[validation_set_len:],
+    # filename
+    learning_rate = 0.01 # needs to be adjusted in the LSTM class!!!
+    layers = 1 # needs to be adjusted in the LSTM class!!!
+    optimizer= 'GN' # needs to be adjusted in the LSTM class!!!
+    temperature = 1.0 
+
+    filename = f'{layers}''_layer_'f'{NR_EPOCHS}''_epoch_'f'{BATCH_SIZE}''_batchsize_'f'{learning_rate}''_eta_'f'{optimizer}'f'_optimizer_'f'{temperature}'f'_temperature'
+
+    dataset_input, dataset_target = DataGenerator.create_array_dataset(book_as_ind[test_set_len:int(0.05*len(book_as_ind))],
                                                                        SEQ_LENGTH)  # arrays of size nr_seq x SEQ_LENGTH-1
     val_input, val_target = DataGenerator.create_array_dataset(book_as_ind[:validation_set_len],
                                                                SEQ_LENGTH)  # arrays of size nr_seq x SEQ_LENGTH-1
+    test_input, test_target = DataGenerator.create_array_dataset(book_as_ind[validation_set_len:test_set_len],
+                                                            SEQ_LENGTH)  # arrays of size nr_seq x SEQ_LENGTH-1
 
     history = lstm.train_network(dataset_input[len(dataset_input) % BATCH_SIZE:],
                                  dataset_target[len(dataset_target) % BATCH_SIZE:], NR_EPOCHS, BATCH_SIZE, val_input,
                                  val_target)
+    
+    # Evaluate the model
+    test_loss, accuracy = lstm.evaluate(x=test_input, y=test_target, bs= BATCH_SIZE)
+    gen_text = lstm.generate_text(temperature, start_string=" ", data_converter=data_converter)
+    
+    print("Test loss:", test_loss, '\n', "Accuracy:", accuracy, '\n', gen_text )
+
+    with open('results/'f'{filename}''.txt', 'a') as f:
+        # Append the loss value followed by a newline character
+        f.write(f'{filename}'+'\n'+ "Test loss:" + str(test_loss) + '\n' + "Accuracy:" + str(accuracy) + '\n' + "Generated Text:" + str(gen_text)+ '\n')
+
+    Evaluator.plot_history_loss(history, filename)
+
+
 
     # dataset = DataGenerator.create_tf_dataset(book_as_ind[validation_set_len: len(book_as_ind)], SEQ_LENGTH)
     # dataset_val = DataGenerator.create_tf_dataset(book_as_ind[:validation_set_len], SEQ_LENGTH)
     # history = lstm.train_network_with_tf_dataset(dataset, NR_EPOCHS, dataset_val)
-
-    Evaluator.plot_history_loss(history)
-
-    print(lstm.generate_text(start_string=" ", data_converter=data_converter))
